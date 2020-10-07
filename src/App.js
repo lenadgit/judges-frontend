8d99faf54f725fd70d95865a22f3d9632273e868
Lena <lena.boruslav@gmail.com>
2020-10-07 21:59:18 +0300

    add menu

--- src/App.js
+++ src/App.js
@@ -7,6 +7,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      
+        <nav className = "Header-menu">
+          <div className = "Logo-side">
+          <span className = "Logo-text-j">Judges</span>&nbsp;
+          <span className = "Logo-text-d">Data</span>
+          </div>
+          <div className = "Link-side">
+          <a href = "https://google.com" className = "Header-link">Головна</a>
+          <a href = "https://google.com" className = "Header-link">Про проект</a>
+          <a href = "https://google.com" className = "Header-link">Дані про суддів</a>
+          </div>  
+        </nav>
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
