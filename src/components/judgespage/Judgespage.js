a35e39183359422dc118fc52cd912f8920b867f0
Lena <lena.boruslav@gmail.com>
2020-10-13 22:25:58 +0300

    some changes

--- src/components/judgespage/Judgespage.js
+++ src/components/judgespage/Judgespage.js
@@ -1,14 +1,21 @@
 import React from "react";
 import Checkbox from "../../components/filterpanel/Checkbox";
 import Autocomplete from "../../components/filterpanel/Autocomplete";
+import JudgesView from "../../components/judgesview/Judgesview";
+
 
 export default function JudgesPage(props) {
 
 
   return (
+    <div className = "judges-wrapper">
     <div>
         <Autocomplete />
         <Checkbox />
+    </div>
+    <div>
+      <JudgesView />
+    </div>
     </div>
   );
 }
---
+++ src/components/judgesview/Judgesview.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Judgesview.css";
+
+
+export default function JudgesView(props) {
+
+
+  return (
+    <div className = "result-wrapper">
+        Результати
+    </div>
+  );
+}
